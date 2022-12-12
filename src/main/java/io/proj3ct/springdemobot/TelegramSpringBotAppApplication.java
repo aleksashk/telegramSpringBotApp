84f893b2ef8ce41b94721cfc5819616854dc5f9a
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-12-12 13:40:43 +0300

    some refactoring

--- src/main/java/io/proj3ct/springdemobot/TelegramSpringBotAppApplication.java
+++ src/main/java/io/proj3ct/springdemobot/TelegramSpringBotAppApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TelegramSpringBotAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TelegramSpringBotAppApplication.class, args);
+        try {
+            SpringApplication.run(TelegramSpringBotAppApplication.class, args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/main/java/io/proj3ct/springdemobot/service/TelegramBot.java
+++ src/main/java/io/proj3ct/springdemobot/service/TelegramBot.java
@@ -7,27 +7,21 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
-
     final BotConfig config;
-
     public TelegramBot(BotConfig config) {
         this.config = config;
     }
-
     @Override
     public String getBotUsername() {
         return config.getBotName();
     }
-
     @Override
     public String getBotToken() {
         return config.getToken();
     }
-
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
@@ -44,13 +38,11 @@ public class TelegramBot extends TelegramLongPollingBot {
             }
         }
     }
-
     private void startCommandReceived(long chatId, String name) {
         String answer = "Hi, " + name + ", nice to meet you!!!";
         log.info("Replied to user " + name);
         sendMessage(chatId, answer);
     }
-
     private void sendMessage(long chatId, String textToSend) {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
